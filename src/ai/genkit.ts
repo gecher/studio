34aabaa6ac8e087bb69fdda39a8f8fea2e9a2695
Getacher Ashebir <getacherashebir@gmail.com>
2025-05-07 07:06:52 +0000

    here is my api key  so use AIzaSyDpWpX_hqcW5iTYCm6AXq9dHBiLPAHQ8wc

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,9 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY || 'AIzaSyDpWpX_hqcW5iTYCm6AXq9dHBiLPAHQ8wc'})],
   model: 'googleai/gemini-2.0-flash',
 });
+
